a64027a018ef5bbd7afda75f3236de4ea8740696
howdyAnkit <palankit35@gmail.com>
2023-11-30 22:24:49 +0530

    Initial Log

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,44 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.\n");
+
+        // Turn 1
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: 1 cow.\n");
+
+        // Turn 2
+        System.out.println("Turn 2. Answer:");
+        System.out.println("5678");
+        System.out.println("Grade: 1 cow.\n");
+
+        // Turn 3
+        System.out.println("Turn 3. Answer:");
+        System.out.println("9012");
+        System.out.println("Grade: 1 bull and 1 cow.\n");
+
+        // Turn 4
+        System.out.println("Turn 4. Answer:");
+        System.out.println("9087");
+        System.out.println("Grade: 1 bull and 1 cow.\n");
+
+        // Turn 5
+        System.out.println("Turn 5. Answer:");
+        System.out.println("1087");
+        System.out.println("Grade: 1 cow.\n");
+
+        // Turn 6
+        System.out.println("Turn 6. Answer:");
+        System.out.println("9205");
+        System.out.println("Grade: 3 bulls.\n");
+
+        // Turn 7
+        System.out.println("Turn 7. Answer:");
+        System.out.println("9305");
+        System.out.println("Grade: 4 bulls.\n");
+        System.out.println("Congrats! The secret code is 9305.");
+
+    }
+}
